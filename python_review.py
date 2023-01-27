0117ec0d3c938e53e6446a74f19882fd7507a1ff
Emily Evans <eevans@MacBook-Air-70.local>
2023-01-27 10:20:23 -0600

    initial commit

---
+++ python_review.py
@@ -0,0 +1,24 @@
+print("hello world")
+print("\n\n\n\n\n\n")
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+
+sum = number_1 + number_2
+print(sum)
+
+if sum > 10:
+	print("boooo")
+elif sum <= 9:
+	print("boooooooooo")
+else: #must be the value 10
+	print("Your number is 10!")
+
+"""
+str() bool() float()
+to comment a large section of code, command ?
+when you do a range of #'s, it'll print 0 to the number before it
+"""
+
+for i in range(sum):
+	print(i)
+
